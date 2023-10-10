f6978a35d8f1d5766d29be56a8495351dace60b1
Johannes Leugering <johannes.leugering@gmail.com>
2023-10-10 10:30:24 -0700

    removed unpacked arrays from ports because yosys doesn't support it

--- src/tt_um_jleugeri_ttt_network.v
+++ src/tt_um_jleugeri_ttt_network.v
@@ -10,7 +10,7 @@ module tt_um_jleugeri_ttt_mux #(
     input logic go_in,
     input logic next_in,
     // mux inputs
-    input logic [1:0] t_startstop_in[NUM_PROCESSORS-1:0],
+    input logic [2*NUM_PORCESSORS-1:0] t_startstop_in,
     // mux outputs
     output logic [$log2(NUM_PROCESSORS)-1:0] idx_out,
     output logic t_start_out,
@@ -33,10 +33,10 @@ module tt_um_jleugeri_ttt_network #(
     parameter int NUM_PROCESSORS = 10
 ) (
     // event start/stop inputs
-    input logic [1:0] tstartstop[NUM_PROCESSORS-1:0],
+    input logic [2*NUM_PORCESSORS-1:0] tstartstop,
     // good/bad event count outputs
-    output logic [NEW_TOKENS_BITS-1:0] new_good_tokens[NUM_PROCESSORS-1:0],
-    output logic [NEW_TOKENS_BITS-1:0] new_bad_tokens[NUM_PROCESSORS-1:0],
+    output logic [NUM_PORCESSORS*NEW_TOKENS_BITS-1:0] new_good_tokens,
+    output logic [NUM_PORCESSORS*NEW_TOKENS_BITS-1:0] new_bad_tokens,
     // control signal
     output logic enable
 );
